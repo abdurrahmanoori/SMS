cdddfb2cacb10b5540c1e73b92eca9fdb5ee937e
Abdurrahman <Abdurrahmannoori10@gmail.com>
2024-08-30 16:15:46 +0430

    survey entity created

---
+++ Entities/Survey.cs
@@ -0,0 +1,10 @@
+﻿namespace Entities
+{
+    public class Survey
+    {
+        public int SurveyID { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public bool IsActive { get; set; }
+
+    }
+}
