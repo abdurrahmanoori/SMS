8716318a616b669ff98ae5f3cc769ad51ed62cd7
muslimhassan1 <145869967+muslimhassan1@users.noreply.github.com>
2024-08-31 11:01:27 +0430

    Update Test.cs

--- Entities/Test.cs
+++ Entities/Test.cs
@@ -1,1 +1,11 @@
-
+namespace Entities
+{
+    public class Test
+    {
+        public int TestId { get; set; }
+        public string? Title { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public int? Duration { get; set; }
+        
+    }
+}
