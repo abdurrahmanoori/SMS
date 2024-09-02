f42e2a387864f5ab9da890a71d06c6cbbe93af51
Abdurrahman <Abdurrahmannoori10@gmail.com>
2024-09-02 05:03:59 +0430

    Generic Repository and unitOrWork implemented

---
+++ Repositories/Generic/UnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using Entities.Data;
+using RepositoryContracts.Generic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.Generic
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _db;
+        // private IPatientTypeRepository _patientTypeRepository;
+
+
+        public UnitOfWork(ApplicationDbContext db)
+        {
+            _db = db;
+            // ProvinceRepository = new ProvinceRepository(db);
+        }
+
+
+        public async Task SaveChanges(CancellationToken cancellationToken)
+        {
+            await _db.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
---
+++ RepositoryContracts/Generic/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryContracts.Generic
+{
+    public interface IUnitOfWork
+    {
+        //IProvinceRepository ProvinceRepository { get; }
+        Task SaveChanges(CancellationToken cancellationToken);
+
+    }
+}
