e651ef9135f7043b400cb3bb466112dcd9e45580
muslimhassan1 <145869967+muslimhassan1@users.noreply.github.com>
2024-09-05 08:52:27 +0430

    Update SurveyInstance.cs

--- Entities/SurveyInstance.cs
+++ Entities/SurveyInstance.cs
@@ -7,5 +7,8 @@ namespace Entities
         public DateTime ActivationDate { get; set; }
         public DateTime DeactivationDate { get; set; }
         public bool IsActive { get; set; }
+        public Survey Survey { get; set; }
+        public ICollection<SurveyInstanceQuestion> SurveyInstanceQuestions { get; set; }
+        public ICollection<SurveyResponse> SurveyResponses { get; set; }
     }
 }
