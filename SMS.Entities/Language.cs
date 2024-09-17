199620cd247a5b703f6dbff1d7ff8813154ce05d
Abdurrahman <Abdurrahmannoori10@gmail.com>
2024-09-17 16:49:03 +0430

    someUselessPropertyRemovedFromLanguae

--- SMS.Entities/Language.cs
+++ SMS.Entities/Language.cs
@@ -1,20 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SMS.Entities
+﻿namespace SMS.Entities
 {
 
     public class Language
     {
         public int LanguageID { get; set; }
-        public string LanguageCode { get; set; }  // e.g., "en", "fa", "ur"
-        public bool IsActive { get; set; }
-        public DateTime CreatedDate { get; set; }
-        public string LanguageDescription { get; set; }
-
+        public string? LanguageCode { get; set; }  // e.g., "en", "fa", "ur"
+        public string LanguageName { get; set; }
+       // public bool IsActive { get; set; }
+        //public DateTime CreatedDate { get; set; }
+        //public string LanguageDescription { get; set; }
      
     }
 }
