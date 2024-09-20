7b9c584449362a1ba80c0ec102df021faeb03596
muslimhassan1 <145869967+muslimhassan1@users.noreply.github.com>
2024-09-20 08:46:00 +0430

    Some properties added to Respondent entity.

--- SMS.Entities/Respondent.cs
+++ SMS.Entities/Respondent.cs
@@ -1,19 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SMS.Entities
+﻿namespace SMS.Entities
 {
 
     public class Respondent
     {
         public int RespondentID { get; set; }
-        public string Name { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
         public string Email { get; set; }
-        public string Province { get; set; }  // Example: "Kabul", "Tehran", etc.
-
-        public ICollection<SurveyResponse> SurveyResponses { get; set; }
+        public string PhoneNumber { get; set; }
+        public int DateOfBirth { get; set; }
+        public string Province { get; set; }
+        public string EducationLevel { get; set; }
     }
 }
