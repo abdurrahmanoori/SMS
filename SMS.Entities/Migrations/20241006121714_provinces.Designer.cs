eec096d6afab5440d419a866871f0ee03fb5ca94
AbubakrKaakar24 <abubakr.kaakar.2017@gmail.com>
2024-10-06 17:20:24 +0500

    Province Migration is done.

--- SMS.Entities/Migrations/20241006121714_provinces.Designer.cs
+++ SMS.Entities/Migrations/20241006121714_provinces.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Entities.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace SMS.Entities.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241006121714_provinces")]
+    partial class provinces
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "6.0.0");
