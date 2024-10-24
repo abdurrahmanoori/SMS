cc47fcc9156b5273363962ac1e3982b5fccd2758
Muslim Hassan <muslimhassan@gmail.com>
2024-10-24 11:43:47 +0430

    Province & District Seeders have been created

---
+++ SMS.Entities/DatabaseSeeders/DistrictSeed.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SMS.Entities.DatabaseSeeders
+{
+    public static class DistrictSeed
+    {
+        public static void DataSeed(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<District>().HasData(
+             new District
+             {
+                 DistrictId = 1,
+                 DistrictName = "District A",
+                 ProvinceId = 1
+             },
+            new District
+            {
+                DistrictId = 2,
+                DistrictName = "District B",
+                ProvinceId = 1
+            },
+            new District
+            {
+                DistrictId = 3,
+                DistrictName = "District C",
+                ProvinceId = 2
+
+            });
+        }
+    }
+}
